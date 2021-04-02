f08e288d8b389746bd5d3bdf2df7e12b271cebd4
Pyry Heiskanen <pyry.heiskanen@iki.fi>
2021-04-02 17:03:36 +0300

    Add more packages

--- init.lua
+++ init.lua
@@ -9,20 +9,39 @@ if fn.empty(fn.glob(install_path)) > 0 then
 end
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
   use 'wbthomason/packer.nvim'
-  use {
-    'savq/melange',
-    config = 'vim.cmd[[colo melange]]'
-  }
   use 'justinmk/vim-dirvish'
-  use 'mhinz/vim-startify'
+
+  use {
+    'mhinz/vim-startify',
+    config = function()
+      vim.g['startify_change_to_vcs_root'] = 1
+      vim.g['startify_change_to_dir'] = 0
+    end
+  }
+
   use 'junegunn/goyo.vim'
 
+  use 'tpope/vim-repeat'
   use 'tpope/vim-eunuch'
   use 'tpope/vim-rsi'
   use 'tpope/vim-abolish'
-  use 'tpope/vim-fugitive'
+  use 'tpope/vim-surround'
+  use 'tpope/vim-commentary'
+  use {
+    'tpope/vim-fugitive',
+    config = function() vim.cmd [[
+      nnoremap <leader>gdm :Gdiff master<CR>
+      nnoremap <leader>gdi :Gdiff<CR>
+      nnoremap <leader>gs :Gstatus<CR>
+      nnoremap <leader>gb :Gblame<CR>
+      nnoremap <leader>gw :Gwrite<CR>
+      nnoremap <leader>gr :Ggrep<Space>
+      nnoremap <leader>gc :Gcommit<CR>
+    ]] end
+  }
+
+  use 'preservim/vimux'
 
   use {
     'nvim-telescope/telescope.nvim',
@@ -35,7 +54,166 @@ return require('packer').startup(function(use)
       nnoremap <leader>fq <cmd>Telescope quickfix<cr>
       nnoremap <leader>fm <cmd>Telescope marks<cr>
       nnoremap ; <cmd>Telescope git_files<cr>
-    ]]
+    ]] end
+  }
+
+  use {
+    'norcalli/nvim-colorizer.lua',
+    config = function() require'colorizer'.setup() end
+  }
+
+  -- js/ts
+  use 'pangloss/vim-javascript'
+  use 'MaxMEllon/vim-jsx-pretty'
+  use 'HerringtonDarkholme/yats.vim'
+
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = function() vim.cmd [[
+      TSUpdate
+    ]] end,
+    config = function()
+      require'nvim-treesitter.configs'.setup {
+        ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+        highlight = {
+          enable = true,
+        },
+      }
+    end
+  }
+
+  use {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    requires = { 'nvim-treesitter/nvim-treesitter' },
+    config = function()
+      require'nvim-treesitter.configs'.setup {
+        textobjects = {
+          select = {
+            enable = true,
+            keymaps = {
+              -- You can use the capture groups defined in textobjects.scm
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              ["ic"] = "@class.inner",
+
+              -- Or you can define your own textobjects like this
+              ["iF"] = {
+                python = "(function_definition) @function",
+                cpp = "(function_definition) @function",
+                c = "(function_definition) @function",
+                java = "(method_declaration) @function",
+              },
+            },
+          },
+          swap = {
+            enable = true,
+            swap_next = {
+              ["<leader>s"] = "@parameter.inner",
+            },
+            swap_previous = {
+              ["<leader>S"] = "@parameter.inner",
+            },
+          },
+          move = {
+            enable = true,
+            goto_next_start = {
+              ["]]"] = "@function.outer",
+              ["]c"] = "@class.outer",
+            },
+            goto_next_end = {
+              ["]["] = "@function.outer",
+              ["]C"] = "@class.outer",
+            },
+            goto_previous_start = {
+              ["[["] = "@function.outer",
+              ["[c"] = "@class.outer",
+            },
+            goto_previous_end = {
+              ["[]"] = "@function.outer",
+              ["[C"] = "@class.outer",
+            },
+          },
+        },
+      }
+    end
+  }
+
+  use {
+    'neoclide/coc.nvim',
+    branch = 'release',
+    run = function() vim.cmd [[
+      CocInstall coc-tsserver
+    ]] end,
+    config = function() vim.cmd [[
+      autocmd CursorHold * silent call CocActionAsync('highlight')
+
+      nmap <leader>ak <Plug>(coc-diagnostic-prev)
+      nmap <leader>aj <Plug>(coc-diagnostic-next)
+
+      nmap <silent> gd <Plug>(coc-definition)
+      nmap <silent> gy <Plug>(coc-type-definition)
+      nmap <silent> gi <Plug>(coc-implementation)
+      nmap <silent> gr <Plug>(coc-references)
+
+      nnoremap <silent> K :call <SID>show_documentation()<CR>
+      function! s:show_documentation()
+        if (index(['vim','help'], &filetype) >= 0)
+          execute 'h '.expand('<cword>')
+        else
+          call CocAction('doHover')
+        endif
+      endfunction
+   ]] end
+  }
+
+  use {
+    'TimUntersberger/neogit',
+    config = function()
+      local neogit = require('neogit')
+      neogit.setup {}
+    end
+  }
+
+  use {
+    "npxbr/gruvbox.nvim",
+    requires = {"rktjmp/lush.nvim"},
+    -- config = 'vim.cmd[[colo gruvbox]]'
+  }
+
+  use {
+    'savq/melange',
+    config = 'vim.cmd[[colo melange]]'
+  }
+
+  use "tversteeg/registers.nvim"
+
+  use {
+    'npxbr/glow.nvim',
+    config = function() vim.cmd [[
+      nnoremap <leader>p :Glow<CR>
+    ]] end,
+  }
+
+  use {
+    'lewis6991/gitsigns.nvim',
+    requires = {
+      'nvim-lua/plenary.nvim'
+    },
+    config = function()
+      require('gitsigns').setup()
     end
   }
 end)
+
+
+-- text objects tree sitter, this seems good
+-- https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+--
+-- https://github.com/hrsh7th/vim-vsnip
+--
+--
+-- lua first git signs this looks good
+-- https://github.com/lewis6991/gitsigns.nvim
+--
+-- https://github.com/phaazon/hop.nvim
