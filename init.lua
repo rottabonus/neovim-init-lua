f6bcdbafc285194dc1eb7f2540b47784d6cfa602
Felix Hallenberg <feqwin@gmail.com>
2022-12-26 11:07:11 +0200

    Improve neoclide-conf

--- init.lua
+++ init.lua
@@ -130,7 +130,7 @@ return require('packer').startup(function(use)
         end,
         config = function()
             require'nvim-treesitter.configs'.setup {
-                ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+                ensure_installed = maintained, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
                 highlight = {enable = true}
             }
         end
@@ -224,6 +224,34 @@ return require('packer').startup(function(use)
       nmap <silent> gi <Plug>(coc-implementation)
       nmap <silent> gr <Plug>(coc-references)
       nmap <silent> K :call CocActionAsync('doHover')<cr>
+
+      " Use tab for trigger completion with characters ahead and navigate
+      " NOTE: There's always complete item selected by default, you may want to enable
+      " no select by `"suggest.noselect": true` in your configuration file
+      " NOTE: Use command ':verbose imap <tab>' to make sure tab is not mapped by
+      " other plugin before putting this into your config
+      inoremap <silent><expr> <TAB>
+            \ coc#pum#visible() ? coc#pum#next(1) :
+            \ CheckBackspace() ? "\<Tab>" :
+            \ coc#refresh()
+      inoremap <expr><S-TAB> coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"
+
+      " Make <CR> to accept selected completion item or notify coc.nvim to format
+      " <C-g>u breaks current undo, please make your own choice
+      inoremap <silent><expr> <CR> coc#pum#visible() ? coc#pum#confirm()
+                              \: "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"
+
+      function! CheckBackspace() abort
+          let col = col('.') - 1
+          return !col || getline('.')[col - 1]  =~# '\s'
+      endfunction
+
+      " Use <c-space> to trigger completion
+      if has('nvim')
+        inoremap <silent><expr> <c-space> coc#refresh()
+      else
+        inoremap <silent><expr> <c-@> coc#refresh()
+      endif
    ]]
         end
     }
